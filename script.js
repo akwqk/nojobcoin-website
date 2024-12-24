1f792deeda12b7df7140b9407a4072674826701d
akwqk <108535025+akwqk@users.noreply.github.com>
2024-12-24 03:52:44 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-function startJourney() { document.querySelector('.earth-scene').style.display = 'none'; document.querySelector('.space-scene').style.display = 'flex'; }
-function continueJourney() { document.querySelector('.space-scene').style.display = 'none'; document.querySelector('.moon-scene').style.display = 'flex'; }
-function togglePlanet(planet) { document.getElementById(planet).classList.toggle('active'); }
-    
